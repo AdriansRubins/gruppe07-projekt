28254e764b544c8038ae5ddb6b81f6f25d694759
vickysharma0601 <vic.sharma.21@lehre.mosbach.dhbw.de>
2022-03-24 15:54:45 +0100

    index.html

--- php/templates/header.php
+++ php/templates/header.php
@@ -1,1 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="fontawesome-free-6.1.0-web\css\all.css">
+    <link rel= "stylesheet" href="../../css/header.css">
+    <title>Header</title>
+</head>
+<header>
+
+    <h1 id="company">Auto-Care!</h1>
+    <nav>
+
+        <ul class="nav__links">
+            <li><a href="#"><button>Startseite</button></a></li>
+            <li><a href="#"><button>Leistungen</button></a></li>
+            <li><a href="#"><button>Autos</button></a></li>
+            <li><a href="#"><button>Kontakt</button></a></li>
+        </ul>
+    </nav>
+
+</header>
+
+<body>
+
+
+</body>
+</html>
