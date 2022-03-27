4bfa750108a888b222353a8b6478cf41d210eb12
vickysharma0601 <vic.sharma.21@lehre.mosbach.dhbw.de>
2022-03-27 20:04:07 +0200

    index.html

--- php/templates/footer.php
+++ php/templates/footer.php
@@ -5,20 +5,19 @@
 
     <footer>
         <div class="footer-content">
-            <h3>Car-care!</h3>
-            <br>
-            <p>The only auto dealer in Duale Hochschule Baden-Württemberg Mosbach Campus Nad Mergentheim</p>
-            <br>
+            <h3 id="car-care">Auto-care!</h3>
+
+            <p>The only auto dealer in Duale Hochschule Baden-Württemberg Mosbach Campus Bad Mergentheim</p>
+
             <ul class="socials">
                 <li><i class="fa fa-google-plus"></i></li>
                 <li><i class="fa fa-facebook-f"></i></li>
-                <li><i class="fa fa-youtube"></i></li>
                 <li><i class="fa fa-twitter"></i></li>
                 <li><i class="fa fa-pinterest"></i></li>
             </ul>
         </div>
         <div class="footer-bottom">
-            <p>copyright &copy;2022 Auto-Care. designed by <span>Vicky</span></p>
+            <p>copyright &copy;2022 Auto-Care. designed by <span>Group 7</span></p>
         </div>
     </footer>
 </div>
@@ -41,10 +40,83 @@
         justify-content: center;
         flex-direction: column;
         text-align: center;
+        color:white;
+        box-shadow: 2px 2px 50px 2px #2660B6;
 
 
     }
+    .footer-content{
+
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        text-align: center;
+
+    }
+    .footer-content h3{
+        font-size: 1.8rem;
+        font-weight: 400;
+        text-transform: capitalize;
+        line-height: 3rem;
+    }
+
+    .footer-content p{
+        max-width: 500px;
+        margin: 10px auto;
+        line-height: 28px;
+        font-size: 14px;
+    }
+
+    .socials{
+        list-style: none;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin: 1rem 0 3rem 0;
 
 
+    }
+    .socials li{
+        margin: 0 10px;
+    }
+    .socials i{
+        font-size: 2rem;
+        transition: color .5s ease;
+    }
+
+    .socials a:hover i{
+        color: aqua;
+    }
+
+    .footer-bottom{
+        background: black;
+        width: 100vw;
+        padding: 20px 0px;
+        text-align: center;
+    }
+    .footer-bottom p{
+        font-size: 14px;
+        word-spacing: 2px;
+        text-transform: capitalize;
+    }
+    .footer-bottom span{
+        text-transform: uppercase;
+        opacity: .4;
+        font-weight: 200;
+    }
+
+    .fa-google-plus:hover{
+        color: #E22A06;
+    }
+    .fa-facebook-f:hover{
+        color: #1E71F5;
+    }
+
+    .fa-twitter:hover{
+        color: #5C94EE;
+    }
+    .fa-pinterest:hover{
+        color: #BB3317;
+    }
 
 </style>
