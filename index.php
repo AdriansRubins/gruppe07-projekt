2ed47584627f10baeabe267dbeac8717b60e3267
vicky-dhbw <101749450+vicky-dhbw@users.noreply.github.com>
2022-03-28 23:29:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,59 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="fontawesome-free-6.1.0-web\css\all.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Acme&family=Play&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./css/style.css">
-    <title>Document</title>
-</head>
-<body>
-<div class="outer">
-    <div class="wrapper">
-        <div class="item item1">
+<?php 
+    $currentPage='index';
+    include ('./php/templates/header.php'); 
+    
+?>
+<div class="servicegrid">
+        <div id="showcase">
+            <h1 id="exp">Explore-</h1>
+            <h1 id="cars">Cars!</h1>
         </div>
-        <div class="item item2">
-            <nav class="main-nav">
-                <ul>
-                    <li><a href="#">Startseite</a></li>
-                    <li><a href="#">Leistungen</a></li>
-                    <li><a href="#">Autos</a></li>
-                    <li><a href="#">Kontakt</a></li>
-
-                </ul>
-            </nav>
+        <div id="about2">
+            <span id="us2">About us!</span>
         </div>
-        <div class="item item3">
+        <div id="des">
+             <p id="text">
+             Auto-care is a fictive website created by Group 7 TINF21 for Web Engineering1 Project, Fall Winter Semester 2021.
+                Auto-care deals everything related to automobiles. With a variety of new and second-hand cars, Auto-care has
+                been one of the leading Auto dealers in Duale Hochschule Mosbach Baden-Württemberg Campus Bad Mergentheim.
+                We provides quite a few services to keep your vehicle retain texture and smooth functioning of mechanical parts.
+                Enjoy the webiste tour!
+             </p>
 
         </div>
-
-    </div>
-
-    <div class="showcase">
-        <div class="bottom-layer">
-            <div class="div left">
-                <img class="background" src="https://wallpapercave.com/wp/wp8881621.jpg" alt="one" height="100%" width="100%">
-            </div>
-            <div class="div right">
-                <img class="background2" src="https://i.pinimg.com/564x/e7/e5/26/e7e526848a9fcb210220deda50028fd7.jpg" alt="one" height="100%" width="100%">
-            </div>
+        <div id="footer">
+        <?php include ('./php/templates/footer.php'); ?>
         </div>
-        <div class="top-layer">
-            <div class="text">
-                <div class="slideFromLeft"><h1>Know-</h1></div>
-                <h1 class="period slideFromRight"> Cars!</h1>
-
-            </div>
-        </div>
-    </div>
 
 </div>
-
-
-
+       
 </body>
 </html>
