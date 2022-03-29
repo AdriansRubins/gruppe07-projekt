d0a34011780481359120160c8a1bf35047bee5f3
AdriansRubins <95778763+AdriansRubins@users.noreply.github.com>
2022-03-29 21:43:40 +0200

    link

--- php/templates/header.php
+++ php/templates/header.php
@@ -5,10 +5,10 @@
     <h1 id="company">Auto-Care!</h1>
     <nav>
         <ul class="nav__links">
-            <li><a  href="../../index.php"><button class="<?php echo $currentPage = 'index' ? 'active' : '' ?>">Startseite</button></a></li>
-            <li><a  href="../pages/leistungen.php"><button class="<?php echo $currentPage = 'leistungen' ? 'active' : '' ?>">Leistungen</button></a></li>
-            <li><a  href="../pages/autos.php"><button class="<?php echo $currentPage = 'autos' ? 'active' : '' ?>">Autos</button></a></li>
-            <li><a  href="../forms/kontakt.php"><button class="<?php echo $currentPage = 'kontakt' ? 'active' : '' ?>">Kontakt</button></a></li>
+            <li><a  href="../../index.php"><button class="<?php echo $currentPage == 'index' ? 'active' : '' ?>">Startseite</button></a></li>
+            <li><a  href="../pages/leistungen.php"><button class="<?php echo $currentPage == 'leistungen' ? 'active' : '' ?>">Leistungen</button></a></li>
+            <li><a  href="../pages/autos.php"><button class="<?php echo $currentPage == 'autos' ? 'active' : '' ?>">Autos</button></a></li>
+            <li><a  href="../forms/kontakt.php"><button class="<?php echo $currentPage == 'kontakt' ? 'active' : '' ?>">Kontakt</button></a></li>
         </ul>
     </nav>
 </header>
