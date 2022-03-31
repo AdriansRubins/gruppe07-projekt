a3cecce4705143f2baf9876775f980d9af4fd9a9
AdriansRubins <95778763+AdriansRubins@users.noreply.github.com>
2022-03-31 22:58:29 +0200

    Fast fertig

--- php/pages/datenschutz.php
+++ php/pages/datenschutz.php
@@ -10,6 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Acme&family=Play&display=swap" rel="stylesheet">
     <link href="stylesheet" href="../../assets/fonts/fontawesome-free-6.1.0-web/css/.brands.css">
     <link rel="stylesheet" href="../../css/footerstyle.css">
+    <link rel="stylesheet" href="../../css/datenschutzUndImpressum.css">
     <link rel="stylesheet" href="../../css/headerstyle.css">
     <link rel="stylesheet" href="../../css/style.css">
     <title>Auto-care!</title>
@@ -20,10 +21,67 @@
     $currentPage='';
     include ('../templates/header.php');
 ?>
-<br>
-
-<div class="dataGrid">
-    <h1>This is Datenschutz page!</h1>
+<div id="datenschutz" class="overlay">
+    <div class="popup">
+        <a class="close" href="#">&times;</a>
+        <h1>Datenschutzerklärung</h1>
+        <h2>Einleitung</h2>
+        <p>Mit der folgenden Datenschutzerklärung möchten wir Sie darüber aufklären, welche Arten Ihrer personenbezogenen Daten (nachfolgend auch kurz als "Daten“ bezeichnet) wir zu welchen Zwecken und in welchem Umfang verarbeiten. Die Datenschutzerklärung gilt für alle von uns durchgeführten Verarbeitungen personenbezogener Daten, sowohl im Rahmen der Erbringung unserer Leistungen als auch insbesondere auf unseren Webseiten, in mobilen Applikationen sowie innerhalb externer Onlinepräsenzen, wie z.B. unserer Social-Media-Profile (nachfolgend zusammenfassend bezeichnet als "Onlineangebot“).</p>
+        <p>Die verwendeten Begriffe sind nicht geschlechtsspezifisch.</p>
+        <p>Stand: 31. März 2022</p>
+        <br>
+        <h2>Verantwortlicher</h2>
+        <p>Jeremias Sullivan, Greis<br>Datenschutzstraße, 41<br>97980,  Bad Mergentheim, Baden-Württemberg, Deutschland</p>
+        E-Mail-Adresse:
+        <p><a href="mailto:jer.greis.21@lehre.mosbach.dhbw.de">jer.greis.21@lehre.mosbach.dhbw.de</a></p>
+        <br>
+        <h2>Übersicht der Verarbeitungen</h2>
+        <p>Die nachfolgende Übersicht fasst die Arten der verarbeiteten Daten und die Zwecke ihrer Verarbeitung zusammen und verweist auf die betroffenen Personen.</p>
+        <h3>Arten der verarbeiteten Daten</h3>
+        <ul>
+            <li>Bestandsdaten.</li>
+            <li>Kontaktdaten.</li>
+            <li>Inhaltsdaten.</li>
+        </ul>
+        <h3>Kategorien betroffener Personen</h3>
+        <ul>
+            <li>Kommunikationspartner.</li>
+        </ul>
+        <h3>Zwecke der Verarbeitung</h3>
+        <ul>
+            <li>Erbringung vertraglicher Leistungen und Kundenservice.</li>
+            <li>Kontaktanfragen und Kommunikation.</li>
+        </ul>
+        <h3>Maßgebliche Rechtsgrundlagen</h3>
+        <p>Im Folgenden erhalten Sie eine Übersicht der Rechtsgrundlagen der DSGVO, auf deren Basis wir personenbezogene Daten verarbeiten. Bitte nehmen Sie zur Kenntnis, dass neben den Regelungen der DSGVO nationale Datenschutzvorgaben in Ihrem bzw. unserem Wohn- oder Sitzland gelten können. Sollten ferner im Einzelfall speziellere Rechtsgrundlagen maßgeblich sein, teilen wir Ihnen diese in der Datenschutzerklärung mit.</p>
+        <ul>
+            <li><strong>Vertragserfüllung und vorvertragliche Anfragen (Art. 6 Abs. 1 S. 1 lit. b. DSGVO)</strong><p> - Die Verarbeitung ist für die Erfüllung eines Vertrags, dessen Vertragspartei die betroffene Person ist, oder zur Durchführung vorvertraglicher Maßnahmen erforderlich, die auf Anfrage der betroffenen Person erfolgen.</p></li>
+            <li><strong>Rechtliche Verpflichtung (Art. 6 Abs. 1 S. 1 lit. c. DSGVO)</strong><p> - Die Verarbeitung ist zur Erfüllung einer rechtlichen Verpflichtung erforderlich, der der Verantwortliche unterliegt.</p></li>
+            <li><strong>Berechtigte Interessen (Art. 6 Abs. 1 S. 1 lit. f. DSGVO)</strong><p> - Die Verarbeitung ist zur Wahrung der berechtigten Interessen des Verantwortlichen oder eines Dritten erforderlich, sofern nicht die Interessen oder Grundrechte und Grundfreiheiten der betroffenen Person, die den Schutz personenbezogener Daten erfordern, überwiegen.</p></li>
+        </ul>
+        <p>Zusätzlich zu den Datenschutzregelungen der Datenschutz-Grundverordnung gelten nationale Regelungen zum Datenschutz in Deutschland. Hierzu gehört insbesondere das Gesetz zum Schutz vor Missbrauch personenbezogener Daten bei der Datenverarbeitung (Bundesdatenschutzgesetz – BDSG). Das BDSG enthält insbesondere Spezialregelungen zum Recht auf Auskunft, zum Recht auf Löschung, zum Widerspruchsrecht, zur Verarbeitung besonderer Kategorien personenbezogener Daten, zur Verarbeitung für andere Zwecke und zur Übermittlung sowie automatisierten Entscheidungsfindung im Einzelfall einschließlich Profiling. Des Weiteren regelt es die Datenverarbeitung für Zwecke des Beschäftigungsverhältnisses (§ 26 BDSG), insbesondere im Hinblick auf die Begründung, Durchführung oder Beendigung von Beschäftigungsverhältnissen sowie die Einwilligung von Beschäftigten. Ferner können Landesdatenschutzgesetze der einzelnen Bundesländer zur Anwendung gelangen.</p>
+        <br>
+        <h2>Kontakt- und Anfragenverwaltung</h2>
+        <p>Bei der Kontaktaufnahme mit uns (z.B. per Kontaktformular, E-Mail, Telefon oder via soziale Medien) sowie im Rahmen bestehender Nutzer- und Geschäftsbeziehungen werden die Angaben der anfragenden Personen verarbeitet soweit dies zur Beantwortung der Kontaktanfragen und etwaiger angefragter Maßnahmen erforderlich ist.</p>
+        <p>Die Beantwortung der Kontaktanfragen sowie die Verwaltung von Kontakt- und Anfragedaten im Rahmen von vertraglichen oder vorvertraglichen Beziehungen erfolgt zur Erfüllung unserer vertraglichen Pflichten oder zur Beantwortung von (vor)vertraglichen Anfragen und im Übrigen auf Grundlage der berechtigten Interessen an der Beantwortung der Anfragen und Pflege von Nutzer- bzw. Geschäftsbeziehungen.</p>
+        <ul>
+            <li><strong>Verarbeitete Datenarten:</strong> Bestandsdaten (z.B. Namen, Adressen); Kontaktdaten (z.B. E-Mail, Telefonnummern); Inhaltsdaten (z.B. Eingaben in Onlineformularen).</li>
+            <li><strong>Betroffene Personen:</strong> Kommunikationspartner.</li>
+            <li><strong>Zwecke der Verarbeitung:</strong> Kontaktanfragen und Kommunikation; Erbringung vertraglicher Leistungen und Kundenservice.</li>
+            <li><strong>Rechtsgrundlagen:</strong> Vertragserfüllung und vorvertragliche Anfragen (Art. 6 Abs. 1 S. 1 lit. b. DSGVO); Berechtigte Interessen (Art. 6 Abs. 1 S. 1 lit. f. DSGVO); Rechtliche Verpflichtung (Art. 6 Abs. 1 S. 1 lit. c. DSGVO).</li>
+        </ul>
+        <p><strong>Weitere Hinweise zu Verarbeitungsprozessen, Verfahren und Diensten:</strong></p>
+        <ul>
+            <li><strong>Kontaktformular: </strong><p>Wenn Nutzer über unser Kontaktformular, E-Mail oder andere Kommunikationswege mit uns in Kontakt treten, verarbeiten wir die uns in diesem Zusammenhang mitgeteilten Daten zur Bearbeitung des mitgeteilten Anliegens. Zu diesem Zweck verarbeiten wir personenbezogene Daten im Rahmen vorvertraglicher und vertraglicher Geschäftsbeziehungen, soweit dies zu deren Erfüllung erforderlich ist und im Übrigen auf Grundlage unserer berechtigten Interessen sowie der Interessen der Kommunikationspartner an der Beantwortung der Anliegen und unserer gesetzlichen Aufbewahrungspflichten.</p></li>
+            </li>
+        </ul>
+        <br>
+        <h2 >Änderung und Aktualisierung der Datenschutzerklärung</h2>
+        <p>Wir bitten Sie, sich regelmäßig über den Inhalt unserer Datenschutzerklärung zu informieren. Wir passen die Datenschutzerklärung an, sobald die Änderungen der von uns durchgeführten Datenverarbeitungen dies erforderlich machen. Wir informieren Sie, sobald durch die Änderungen eine Mitwirkungshandlung Ihrerseits (z.B. Einwilligung) oder eine sonstige individuelle Benachrichtigung erforderlich wird.</p>
+        <p>Sofern wir in dieser Datenschutzerklärung Adressen und Kontaktinformationen von Unternehmen und Organisationen angeben, bitten wir zu beachten, dass die Adressen sich über die Zeit ändern können und bitten die Angaben vor Kontaktaufnahme zu prüfen.</p>
+        <br>
+        <p>Quelle: <a href="https://www.e-recht24.de">e-recht24.de</a></p>
+    </div>
 </div>
 <?php
 include ('../templates/footer.php');
