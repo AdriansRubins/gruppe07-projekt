bb49f2f8bc721a7874ed5b2f999d95018d377f5c
1-B768 <bernhard.schwab98@web.de>
2022-03-31 18:54:32 +0200

    Änderungen in KOntakt.php.
    Header und Footer funktioniert noch nicht und ich werde andere Bilder einsetzen, und eventuell die Anpsprechpartner mit mehr infos erweitern
    DAs sind aber nur Klieinigkeiten.
    
    Ansonsten ist es fertig.

---
+++ php/forms/dynamic_page.php
@@ -0,0 +1,12 @@
+<?php
+echo
+    'Vielen Dank für Ihre Nachricht!<br> 
+    Folgende Daten wurden übertragen:<br>
+    <p>
+    <b>Vorname: </b>' . $_POST["vn_vorname"] .'<br>
+    <b>Nachname: </b>' . $_POST["nn_nachname"] .'<br>
+    <b>Mailadresse: </b>' . $_POST["mail"] .'<br>
+    <b>Telefon: </b>' . $_POST["telefon"] .'<br>
+    <b>Ihre Nachricht: </b>' . $_POST["nachricht"] .'<br>
+    </p>'
+?>
--- php/forms/kontakt.php
+++ php/forms/kontakt.php
@@ -8,50 +8,105 @@ include('./php/templates/header.php');
         <h1>Get in touch!</h1>
     </div>
     <div class="form">
-        <form action="dynamic_page.php" method="get">
-            <h1>Kontaktformular</h1>
-            <hr>
-            <label>Vorname:
-                <input type="text" placeholder="Vorname" name="vn_name">
-            </label>
-            <label>Nachname:
-                <input type="text" placeholder="Nachname" name="vn_name"><br>
-            </label>
-            <label>Mail:
-                <input type="email" placeholder="Mailadresse" name="mail" required>
-            </label>
-            <label>Telefon:
-                <input type="text" placeholder="Telefon" name="vn_name"><br>
-            </label>
-            <label>Nachricht:<br>
-                <textarea placeholder="Nachricht" required name="kommentar"></textarea><br>
-            </label>
-            <button type="submit">Absenden</button><button type="reset">Zurücksetzen</button>
+        <form action="dynamic_page.php" method="post">
+            <h1>Konkaktformular</h1>
+            <label for="vn_vorname">Vorname:</label><br>
+            <input id="vn_vorname" class="inputfield" type="text" placeholder="Vorname" name="vn_vorname" required><br>
+            <label for="nn_nachname">Nachname:</label><br>
+            <input id="nn_nachname" class="inputfield" type="text" placeholder="Nachname" name="nn_nachname" required><br>
+            <label for="mail">Mailadresse:</label><br>
+            <input id="mail" class="inputfield" type="email" placeholder="Mailadresse" name="mail" required><br>
+            <label for="telefon">Telefon:</label><br>
+            <input id="telefon" class="inputfield" type="text" placeholder="Telefon" name="telefon"><br>
+            <label for="nachricht">Ihre Nachricht:</label><br>
+            <textarea id="nachricht" class="inputfield" placeholder="Ihre Nachricht" required name="nachricht"></textarea><br>
+            <p id="nachricht"><input type="checkbox" required>Ich erkläre mich damit einverstanden, dass meine personenbezogenen Daten nur zum Zwecke
+                der Bearbeitung im Rahmen des Kontaktformulars verarbeitet und gespeichert werden.
+                Die Daten werden nur für die hier aufgeführten Zwecke verwendet und nicht an Dritte weitergegeben.
+            </p>
+            <button class="inputfield" type="submit">Absenden</button><button class="inputfield" type="reset">Zurücksetzen</button>
         </form>
     </div>
     <div class="helper">
-        <h1>Ansprechpartner</h1>
-        <hr>
-        <div>
-            <img class="pictures" src="../../assets/images/1.jpg">
-            <img class="pictures" src="../../assets/images/2.jpg">
-            <img class="pictures" src="../../assets/images/3.jpg">
-            <img class="pictures" src="../../assets/images/4.jpg">
+        <h1 id="headinghelper">Ansprechpartner</h1>
+        <img class="pictures" src="../../assets/images/1.jpg">
+        <img class="pictures" src="../../assets/images/2.jpg">
+            <div class="Textposition">
+                <p>
+                <ul class="text">
+                    <h3 class="text">7239202</h3>
+                    <li>Tel: 01654398936</li>
+                    <li>Email: </li>
+                </ul>
+                </p>
+                <p>
+                <ul>
+                    <h3 class="text">7239202</h3>
+                    <li>Tel: 01654398936</li>
+                    <li>Email: </li>
+                </ul>
+                </p>
+            </div>
+        <img class="pictures" src="../../assets/images/3.jpg">
+        <img class="pictures" src="../../assets/images/4.jpg">
+        <div class="Textposition">
+            <p>
+            <ul class="text">
+                <h3 class="text">7239202</h3>
+                <li>Tel: 01654398936</li>
+                <li>Email: </li>
+            </ul>
+            </p>
+            <p>
+            <ul>
+                <h3 class="text">7239202</h3>
+                <li>Tel: 01654398936</li>
+                <li>Email: </li>
+            </ul>
+            </p>
         </div>
-
-
-
-    </div>
+        </div>
 </div>
 <?php include('./php/templates/footer.php'); ?>
 
-</DOCTYPE>
-
-
 <style>
+    h3{
+        font-size: x-large;
+        font-weight: bold;
+    }
+    li{
+        font-size: large;
+    }
+    ul{
+        width: 30%;
+        margin: 0% 10%;
+    }
+    .Textposition{
+        display: flex;
+    }
+    #headinghelper{
+        text-align: center;
+    }
+    h1{
+        margin-bottom: 10%;
+        font-size: xxx-large;
+    }
+    #nachricht{
+        font-size: x-large;
+        width: 100%;
+    }
+    .inputfield{
+        font-size: x-large;
+        width: 40%;
+    }
+    label{
+        font-size: x-large;
+        width: 40%;
+    }
     .pictures{
-        width: 10%;
-        border: 1px solid rgba(0, 0, 0, 0.38);
+        width: 30%;
+        margin: 0% 9%;
+        border: 2px solid rgba(0, 0, 0, 0.90);
         border-radius: 2px;
     }
     .kontaktgrid{
@@ -73,11 +128,16 @@ include('./php/templates/header.php');
     }
     .form {
         grid-area: form;
-        background: #8AA39B;
+        background-color: #5C6F68;
+        padding: 5%;
+        margin: 0.5% 0.25% 0.5% 0%;
     }
     .helper{
         grid-area: helper;
         background: #95D9C3;
+        background-color: #5C6F68;
+        padding: 5%;
+        margin: 0.5% 0% 0.5% 0.25%;
     }
     .heading{
         font-family: 'Black Han Sans', sans-serif;
@@ -85,5 +145,6 @@ include('./php/templates/header.php');
         text-align: start;
 
     }
+
 
 </style>
