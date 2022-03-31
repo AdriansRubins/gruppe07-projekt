a73bbb8468de06aa290aa1458c25ee4c02d4aff0
vicky-dhbw <101749450+vicky-dhbw@users.noreply.github.com>
2022-03-31 21:00:24 +0200

    Checkout in browser!

--- index.php
+++ index.php
@@ -17,66 +17,77 @@
 </head>
 <body>
 <nav>
-        <input type="checkbox" id="check">
-        <label for="check" class="checkbtn">
-            <i class="fa fa-bars"></i>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+        <i class="fa fa-bars"></i>
 
-        </label>
-        <label class="logo">Auto-care!</label>
-        <ul>
-            <li><a  href="index.php"><button class="<?php echo $currentPage == 'index' ? 'active' : '' ?>">Startseite</button></a></li>
-            <li><a  href="./php/pages/leistungen.php"><button class="<?php echo $currentPage == 'leistungen' ? 'active' : '' ?>">Leistungen</button></a></li>
-            <li><a  href="./php/pages/autos.php"><button class="<?php echo $currentPage == 'autos' ? 'active' : '' ?>">Autos</button></a></li>
-            <li><a  href="./php/forms/kontakt.php"><button class="<?php echo $currentPage == 'kontakt' ? 'active' : '' ?>">Kontakt</button></a></li>
+    </label>
+    <label class="logo">Auto-care!</label>
+    <ul>
+        <li><a  href="index.php"><button class="<?php echo $currentPage == 'index' ? 'active' : '' ?>">Startseite</button></a></li>
+        <li><a  href="./php/pages/leistungen.php"><button class="<?php echo $currentPage == 'leistungen' ? 'active' : '' ?>">Leistungen</button></a></li>
+        <li><a  href="./php/pages/autos.php"><button class="<?php echo $currentPage == 'autos' ? 'active' : '' ?>">Autos</button></a></li>
+        <li><a  href="./php/forms/kontakt.php"><button class="<?php echo $currentPage == 'kontakt' ? 'active' : '' ?>">Kontakt</button></a></li>
 
-        </ul>
-    </nav>
+    </ul>
+</nav>
 
-  
+
 
 <br>
 <div class="servicegrid">
     <div id="showcase">
-        <h1 id="exp">Explore-</h1>
-        <h1 id="cars">Cars!</h1>
+        <h1 id="exp">Explore-Cars</h1>
     </div>
-    <div id="about2">
-        <span id="us2">About us!</span>
-    </div>
-    <div id="des">
-        <p id="text">
-            Auto-care is a fictive website created by Group 7 TINF21 for Web Engineering1 Project, Fall Winter Semester 2021.
-            Auto-care deals everything related to automobiles. With a variety of new and second-hand cars, Auto-care has
-            been one of the leading Auto dealers in Duale Hochschule Mosbach Baden-Württemberg Campus Bad Mergentheim.
-            We provides quite a few services to keep your vehicle retain texture and smooth functioning of mechanical parts.
-            Enjoy the webiste tour!
-        </p>
+</div>
+<div class="wrapper">
+
+    <div class="bottom-layer">
+        <div class="div left"></div>
+        <div class="div right"></div>
 
     </div>
-    <div id="footer">
-        
-    <footer>
-    <div class="footer-content">
-        <h3 class="car-care">Auto-care!</h3>
-
-        <p class="autodealder">The only auto dealer in Duale Hochschule Baden-Württemberg Mosbach Campus Bad Mergentheim</p>
-
-        <ul class="socials">
-            <li><i class="fa fa-google-plus"></i></li>
-            <li><i class="fa fa-facebook-f"></i></li>
-            <li><i class="fa fa-twitter"></i></li>
-            <li><i class="fa fa-pinterest"></i></li>
-        </ul>
-    </div>
-    <div class="footer-bottom">
-        <p>copyright &copy;2022 Auto-Care. designed by <span>Group 7</span></p>
-        <div class="privacy">
-            <a href="./php/pages/impressum.php" class="impressum">Impressum</a> |
-            <a href="./php/pages/datenschutz.php" class="datenschutz">Datenschutz</a>
+    <div class="top-layer">
+        <div class="texti">
+            <div class="FromLeft"><h2 class="about">About</h2></div>
+            <div class="FromRight"><h2 class="period">Us</h2></div>
         </div>
     </div>
-</footer>
-    </div>
+</div>
+<div id="des">
+    <p id="text">
+        Auto-care is a fictive website created by Group 7 TINF21 for Web Engineering1 Project, Fall Winter Semester 2021.
+        Auto-care deals everything related to automobiles. With a variety of new and second-hand cars, Auto-care has
+        been one of the leading Auto dealers in Duale Hochschule Mosbach Baden-Württemberg Campus Bad Mergentheim.
+        We provides quite a few services to keep your vehicle retain texture and smooth functioning of mechanical parts.
+        Enjoy the webiste tour!
+    </p>
+
+</div>
+<div id="footer">
+
+    <footer>
+        <div class="footer-content">
+            <h3 class="car-care">Auto-care!</h3>
+
+            <p class="autodealder">The only auto dealer in Duale Hochschule Baden-Württemberg Mosbach Campus Bad Mergentheim</p>
+
+            <ul class="socials">
+                <li><i class="fa fa-google-plus"></i></li>
+                <li><i class="fa fa-facebook-f"></i></li>
+                <li><i class="fa fa-twitter"></i></li>
+                <li><i class="fa fa-pinterest"></i></li>
+            </ul>
+        </div>
+        <div class="footer-bottom">
+            <p>copyright &copy;2022 Auto-Care. designed by <span>Group 7</span></p>
+            <div class="privacy">
+                <a href="./php/pages/impressum.php" class="impressum">Impressum</a> |
+                <a href="./php/pages/datenschutz.php" class="datenschutz">Datenschutz</a>
+            </div>
+        </div>
+    </footer>
+</div>
 
 </div>
 
