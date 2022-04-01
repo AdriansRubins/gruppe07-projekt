e001ecfb6537a1b7f363bbe5b7b0b6d7b4aa4c1d
vicky-dhbw <101749450+vicky-dhbw@users.noreply.github.com>
2022-04-01 18:09:03 +0200

    leistungen added to main

--- php/pages/leistungen.php
+++ php/pages/leistungen.php
@@ -21,21 +21,100 @@
 $currentPage = 'leistungen';
 include ('../templates/header.php');
 ?>
-<div class="container">
-    <header>Header</header>
-    <div id="card1">
-        <div class="card-logo">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 13.616v-3.232c-1.651-.587-2.694-.752-3.219-2.019v-.001c-.527-1.271.1-2.134.847-3.707l-2.285-2.285c-1.561.742-2.433 1.375-3.707.847h-.001c-1.269-.526-1.435-1.576-2.019-3.219h-3.232c-.582 1.635-.749 2.692-2.019 3.219h-.001c-1.271.528-2.132-.098-3.707-.847l-2.285 2.285c.745 1.568 1.375 2.434.847 3.707-.527 1.271-1.584 1.438-3.219 2.02v3.232c1.632.58 2.692.749 3.219 2.019.53 1.282-.114 2.166-.847 3.707l2.285 2.286c1.562-.743 2.434-1.375 3.707-.847h.001c1.27.526 1.436 1.579 2.019 3.219h3.232c.582-1.636.75-2.69 2.027-3.222h.001c1.262-.524 2.12.101 3.698.851l2.285-2.286c-.744-1.563-1.375-2.433-.848-3.706.527-1.271 1.588-1.44 3.221-2.021zm-12 2.384c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z"/></svg>
-        </div>
-        <div class="card-header">Card Header</div>
+<br>
+<br>
+
+<div class="gesamt">
+
+    <div class="reifenHeading">
+        <h1>Services</h1>
     </div>
-    <div id="card2">Content 2</div>
-    <div id="card3">Content 3</div>
-    <div id="card4">Content 4</div>
-    <div id="card5">Content 5</div>
-    <div id="card6">Content 6</div>
-</div>
-<?php
+
+    <div class="leistungsgrid">
+
+        <div class="serviceRight">
+
+            <div class="reifendiv">
+
+                <details class="styled">
+                    <summary>Reifenservice</summary>
+                    <div class="tyreblock">
+
+                        <div class="reifen">
+                            <p>
+                                für alle gängigen Reifenmarken und Größen <br>
+                                Egal ob Winterreifen, Sommerreifen oder Allwetterreifen, wir machen Ihnen immer ein passendes Angebot.<br>
+                                Über unseren Radkonfigurator wählen wir Ihr Fahrzeug aus und zeigen Ihnen die passenden Alufelgen am Fahrzeug.<br>
+                                Eine eventuell nötige Eintragung wird vor Ort von unserem Partner der GTÜ durchgeführt.<br>
+
+                                Wir beraten Sie gerne!
+                            </p>
+                        </div>
+                        <div class="tyrebild">
+                            <img class="bildtyre" src="../../assets/images/tyres.jpg">
+
+                        </div>
+                    </div>
+
+                </details>
+            </div>
+
+
+            <div class="glasdiv">
+                <details class="glasstyled">
+                    <summary>Autoglas & Glasreparatur</summary>
+                    <div class="glasblock">
+
+                        <div class="glas">
+                            <p>
+                                Nicht jeder Glasschaden darf und kann repariert werden.<br>
+                                Das hängt von der Größe und dem Ort der
+                                Beschädigung ab. Ist die Beschädigung an bestimmten Stellen der Windschutzscheibe, muss die Scheibe
+                                ersetzt werden.<br>
+                                Am besten kommen Sie im Fall der Fälle vorbei, und wir sagen Ihnen, ob repariert werden kann.
+                                Auch der Austausch der Scheibe wird von Ihrer Teilkaskoversicherung abzgl. der Selbstbeteiligung übernommen.<br>
+                                Gerne helfen wir Ihnen bei der Abwicklung mit Ihrer Versicherung.
+                            </p>
+                        </div>
+                        <div class="glasbild">
+                            <img class="bildtyre" src="../../assets/images/glas.jpg">
+
+                        </div>
+                    </div>
+
+                </details>
+            </div>
+
+            <div class="unfalldiv">
+                <details class="unfallstyled">
+                    <summary>Unfallinstandsetzung</summary>
+
+                    <div class="unfallblock">
+
+                        <div class="unfall">
+                            <p>
+                                Von der Beseitigung der kleinen Delle (Smart Repair) bis zur fachmännischen Reparatur eines
+                                Unfallschadens sind wir Ihr qualifizierter Partner. <br>Mit Hilfe des Kalkulationsprogramms Silver
+                                Dat erstellen wir einen Kostenvoranschlag oder beraten Sie, wenn ein Sachverständiger zur Erstellung eines Gutachtens und ein Anwalt nötig ist.<br>
+                                Ein Unfall-Ersatzfahrzeug kann Ihnen bei Bedarf zur Verfügung gestellt werden. Gerne helfen wir Ihnen
+                                bei der Abwicklung mit der Versicherung.<br>Zur Beseitigung von Rahmenschäden verfügen wir über eine Cellette-Richtbank.
+                            </p>
+                        </div>
+                        <div class="unfallbild">
+                            <img class="bildtyre" src="../../assets/images/unfall.jpg" >
+
+                        </div>
+                    </div>
+
+                </details>
+            </div>
+        </div>
+
+
+    </div>
+
+
+    <?php
     include ('../templates/footer.php');
 ?>
 </body>
