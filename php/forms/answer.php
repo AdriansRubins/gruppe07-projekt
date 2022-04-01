807f34a0a18917acf9276b9b59caac16bd2dab4e
AdriansRubins <95778763+AdriansRubins@users.noreply.github.com>
2022-04-01 23:27:26 +0200

    fastfastfast

--- php/forms/answer.php
+++ php/forms/answer.php
@@ -6,7 +6,6 @@ echo
     <b>Vorname: </b>' . $_POST["vn_vorname"] .'<br>
     <b>Nachname: </b>' . $_POST["nn_nachname"] .'<br>
     <b>E-Mail: </b>' . $_POST["mail"] .'<br>
-    <b>Land: </b>' . $_POST["country"] .'<br>
     <b>Telefon: </b>' . $_POST["telefon"] .'<br>
     <b>Ihre Nachricht: </b>' . $_POST["nachricht"] .'<br>
     <b>PLZ: </b>' . $_POST["plz"] .'<br>
