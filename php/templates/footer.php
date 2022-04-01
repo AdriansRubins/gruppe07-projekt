3b3ea528f19e689659930820b5de3a2e862b7e53
AdriansRubins <95778763+AdriansRubins@users.noreply.github.com>
2022-04-01 23:54:06 +0200

    Alles

--- php/templates/footer.php
+++ php/templates/footer.php
@@ -9,10 +9,8 @@
         <p class="autodealder">The only auto dealer in Duale Hochschule Baden-Württemberg Mosbach Campus Bad Mergentheim</p>
 
         <ul class="socials">
-            <li><i class="fa fa-google-plus"></i></li>
-            <li><i class="fa fa-facebook-f"></i></li>
             <li><i class="fa fa-twitter"></i></li>
-            <li><i class="fa fa-pinterest"></i></li>
+            <li><i class="fa fa-instagram"></i></li>
         </ul>
     </div>
     <div class="footer-bottom">
